69d32db2b3ba9819a8dec619328c8c6cab008de5
WissamMU <wessam.2003.ali@gmail.com>
2024-01-05 14:34:23 +0300

    jump one time

--- Assets/CameraPlace.cs
+++ Assets/CameraPlace.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CameraPlace : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform Player;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(Player.position.x,Player.position.y ,transform.position.z);
     }
 }
--- Assets/PlayerMovmint.cs
+++ Assets/PlayerMovmint.cs
@@ -7,13 +7,16 @@ public class PlayerMovmint : MonoBehaviour
 {
     Rigidbody2D rb;
     BoxCollider2D coll;
-    Animation anim;
+    Animator anim;
     SpriteRenderer sprite;
 
+    [SerializeField] LayerMask jumpGround;
 
-    enum state { idel , running , jumping , falling }
 
-    [SerializeField] float jumpForce =8f;
+    enum MovingState { idel , running , jumping , falling }
+
+
+    [SerializeField] float jumpForce =14f;
     [SerializeField] float MovingSpeed = 7f;
     float dirctX = 0f;
     
@@ -24,7 +27,7 @@ public class PlayerMovmint : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         coll = GetComponent<BoxCollider2D>();
-        anim = GetComponent<Animation>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,16 +36,44 @@ public class PlayerMovmint : MonoBehaviour
         dirctX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2 (dirctX * MovingSpeed, rb.velocity.y);
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(0, jumpForce);
+        }
+
         PlayerAnimtion();
     }
 
 
     private void PlayerAnimtion()
     {
-        if (Input.GetButtonDown("Jump"))
+        MovingState state;
+        if (dirctX > 0f)
         {
-            rb.velocity = new Vector2(0, jumpForce);
+            state = MovingState.running;
+            sprite.flipX = false;
         }
-        
+        else if (dirctX < 0f)
+        {
+            state = MovingState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovingState.idel;
+        }
+        if (rb.velocity.y > 0.1f)
+        {
+            state = MovingState.jumping;
+        }
+        else if (rb.velocity.y < -00.1f)
+        {
+            state = MovingState.falling;
+        }
+        anim.SetInteger("state",(int)state);
+    }
+    bool OnGround()
+    {
+        return Physics2D.BoxCast(coll.bounds.center , coll.bounds.size , 0f , Vector2.down , .1f ,jumpGround);
     }
 }
