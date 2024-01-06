8e5160ff6cd24a437f16744c60f4f8b207dd8577
WissamMU <wessam.2003.ali@gmail.com>
2024-01-06 22:23:39 +0300

    next level

--- Assets/code/finshlevel.cs
+++ Assets/code/finshlevel.cs
@@ -5,20 +5,25 @@ using UnityEngine.SceneManagement;
 public class finshlevel : MonoBehaviour
 {
     AudioSource finshingsound;
+    bool LeveledFinshed = false;
+
     void Start()
     {
+        
         finshingsound = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && LeveledFinshed == false)
         {
             finshingsound.Play();
-            finshingLevel();
+            LeveledFinshed=true;
+            Invoke("finshingLevel", 2f);
         }
     }
     private void finshingLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("level 2");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
